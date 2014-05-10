b2d6fbf8da2ed3d54214eeb5d4b904a364ac6fb0
Nemto <nem989@gmail.com>
2014-05-10 17:51:49 +0200

    Initial commit
    It's a mess!

---
+++ Souls/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Souls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_plus5k = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_plus5k
+            // 
+            this.button_plus5k.Location = new System.Drawing.Point(77, 42);
+            this.button_plus5k.Name = "button_plus5k";
+            this.button_plus5k.Size = new System.Drawing.Size(75, 23);
+            this.button_plus5k.TabIndex = 1;
+            this.button_plus5k.Text = "+5000";
+            this.button_plus5k.UseVisualStyleBackColor = true;
+            this.button_plus5k.Click += new System.EventHandler(this.button_plus5k_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(205, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 75);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_plus5k);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Give Me Souls II";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_plus5k;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Souls/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Souls
+{
+    public partial class Form1 : Form
+    {
+
+        #region "Imports"
+        [DllImport("kernel32.dll")]
+        static extern IntPtr OpenProcess(int dwDesiredAccess, [MarshalAs(UnmanagedType.Bool)] bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress,
+        [In, Out] byte[] lpBuffer, UInt32 nSize, ref IntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress,
+        [In, Out] byte[] lpBuffer, UInt32 nSize, ref IntPtr lpNumberOfBytesRead);
+
+        const int PROCESS_ALL_ACCESS = 0x001F0FFF;
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+            Update();
+        }
+
+        private IntPtr darksoulsHandle, baseAddress;
+        private int pid;
+        private Process darksoulsProcess;
+        private byte[] currentSouls = new byte[4];
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct Offset
+        {
+            [FieldOffset(0xE8)]
+            public int souls;
+        }
+
+        Offset offset = new Offset();
+
+        private void Update()
+        {
+            darksoulsProcess = Process.GetProcessesByName("DarkSoulsII").FirstOrDefault();
+            pid = darksoulsProcess.Id;
+            darksoulsHandle = OpenProcess(PROCESS_ALL_ACCESS, false, pid);
+            baseAddress = darksoulsProcess.MainModule.BaseAddress;
+
+            ReadSouls(darksoulsHandle, playerStats() + 0xE8, currentSouls);
+
+            textBox1.Text = BitConverter.ToInt32(currentSouls, 0).ToString();
+        }
+
+        private IntPtr playerStats()
+        {
+            int lvl1 = 0x01004698;
+            int lvl2 = 0x388;
+
+
+            var _buffer = new byte[4];
+
+            ReadSouls(darksoulsHandle, baseAddress + lvl1, _buffer);
+            ReadSouls(darksoulsHandle, (IntPtr)BitConverter.ToInt32(_buffer, 0) + lvl2, _buffer);
+
+            return (IntPtr)BitConverter.ToInt32(_buffer, 0);
+        }
+
+        private static bool WriteSouls(IntPtr pHandle, IntPtr lpAddress, int lpBuffer)
+        {
+            var write = BitConverter.GetBytes(lpBuffer);
+
+            IntPtr _written = new IntPtr(0);
+            return WriteProcessMemory(pHandle, lpAddress, write, (uint)write.Length, ref _written);
+        }
+
+        private static bool ReadSouls(IntPtr pHandle, IntPtr lpAddress, byte[] lpBuffer)
+        {
+            IntPtr _read = new IntPtr(0);
+            return ReadProcessMemory(pHandle, lpAddress, lpBuffer, (uint)lpBuffer.Length, ref _read);
+        }
+
+        private void button_plus5k_Click(object sender, EventArgs e)
+        {
+            WriteSouls(darksoulsHandle, playerStats() + 0xE8, BitConverter.ToInt32(currentSouls, 0) + 5000);
+            Update();
+        }
+    }
+}
